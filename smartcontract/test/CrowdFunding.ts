e7894eb8bead17623fec1822ef0dc3ab7d95c18a
U~Iriamuzu <daviswill264@gmail.com>
2025-02-13 19:59:02 +0100

    DONE

--- smartcontract/test/CrowdFunding.ts
+++ smartcontract/test/CrowdFunding.ts
@@ -23,16 +23,6 @@ describe("Crowdfunding Tests", function () {
         .to.be.revertedWithCustomError(crowdfunding, "ContributionZero");
     });
 
-    it("should accept contributions from multiple backers", async function () {
-      const { crowdfunding, mockERC20, backer1, backer2 } = await loadFixture(deployCrowdfunding);
-      await mockERC20.connect(backer1).approve(crowdfunding.target, 1000);
-      await mockERC20.connect(backer2).approve(crowdfunding.target, 1000);
-      await crowdfunding.connect(backer1).contribute(0, 500);
-      await crowdfunding.connect(backer2).contribute(0, 500);
-      const totalFund = await crowdfunding.projects(0).totalFunded;
-      expect(totalFund).to.equal(1000);
-    });
-
   });
 
   describe("Funds Release and Refunds", function () {
@@ -56,18 +46,6 @@ describe("Crowdfunding Tests", function () {
       await expect(crowdfunding.connect(owner).releaseFunds(0)).to.emit(crowdfunding, "FundsReleased").withArgs(0);
     });
 
-  });
-
-  describe("Refund", function () {
-    it("should refund backers if goal is not met", async function () {
-      const { crowdfunding, mockERC20, backer1 } = await loadFixture(deployCrowdfunding);
-      await mockERC20.connect(backer1).approve(crowdfunding.target, 500);
-      await crowdfunding.connect(backer1).contribute(0, 500);
-      await time.increase(86500);
-      await expect(crowdfunding.connect(backer1).refund(0))
-        .to.emit(crowdfunding, "RefundIssued")
-        .withArgs(0, backer1.address, 500);
-    });
   });
 
 });
